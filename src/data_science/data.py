d3eace87bc0e3642abbac5df380487123353d21b
houssam boukhecham <houssam.boukhecham96@gmail.com>
2023-10-18 18:53:48 +0000

    Update data.py

--- src/data_science/data.py
+++ src/data_science/data.py
@@ -1,47 +1,32 @@
 import pandas as pd
 import numpy as np
 
-
-def split_data(df: pd.DataFrame,
-               test_ratio : float = 0.2,
-               seed : int = 0) -> tuple[pd.DataFrame]:
+def split_data(df: pd.DataFrame, test_ratio: float = 0.2, seed: int = 0) -> tuple:
     """
-    Description:
-        Split the dataset into train and test sets by randomly shuffling the order of the data,
-        while fixing the random seed.
+    Split the dataset into train and test sets by randomly shuffling the order of the data, while fixing the random seed.
 
     Args:
-    ----------
-    df : pandas.DataFrame.
-         The data to be split in train and test sets.
-         
-    test_ratio : float. 
-                 The percentage of the test set. This float must be between 0 and 1.
-        
-    seed : int. 
-           The shuffling parameter.
+        df (pandas.DataFrame): The data to be split into train and test sets.
+        test_ratio (float): The percentage of the test set. Must be between 0 and 1.
+        seed (int): The shuffling parameter.
 
-    Returns
-    ----------
-    X_train, y_train, X_test, y_test : tuple[pd.DataFrame].
- 
+    Returns:
+        tuple: A tuple containing X_train, y_train, X_test, and y_test DataFrames.
     """
-    # Let n be the number of observations we have
+    # Let n be the number of rows of the DataFrame df
     n = df.shape[0]
 
     # We fix the seed to the value seed, which is 0 by default
     np.random.seed(seed)
 
     # The number of elements of the test and train sets.
-    n_test = int( n * test_ratio )
-    
-    if n - n_test <= 0:
-        return("Please choose a valid ratio for the test set")
-       
+    n_test = int(n * test_ratio)
 
-    # We choose at random the indices of the test set.    
-    test_indices = np.random.choice(np.arange(0,n), n_test, replace=False)
-    # test_indices.sort()
+    if n - n_test <= 0:
+        return "Please choose a valid ratio for the test set"
+
+    # We choose at random the indices of the test set.
+    test_indices = np.random.choice(np.arange(0, n), n_test, replace=False)
 
     # To get the indices of the train set, we create an array with values from 0 to n-1, then we remove the values of test_indices array.
     indices = np.arange(n)
@@ -51,9 +36,8 @@ def split_data(df: pd.DataFrame,
     df_train = df.iloc[train_indices]
     df_test = df.iloc[test_indices]
 
-    # From the created dataframes, ....
+    # Here we separate the train and test sets into two subsets containing the explanatory variables and the variable to predict
     X_train, X_test = df_train.drop('price', axis=1), df_test.drop('price', axis=1)
     y_train, y_test = df_train['price'].to_frame(), df_test['price'].to_frame()
 
     return X_train, y_train, X_test, y_test
-
