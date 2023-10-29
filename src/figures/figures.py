9033a0d37760d4cb7d47375ec1f7ab3da0cbbf3a
houssam boukhecham <houssam.boukhecham96@gmail.com>
2023-10-29 08:40:51 +0100

    fig

---
+++ src/figures/figures.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def etude_univarie(data):
+    """
+    Description:
+    This function displays the distribution of each variable.
+    
+    Parameters:
+    data: A DataFrame
+    
+    Returns:
+    Graphical figures
+    """
+    colonne_quanti = data.select_dtypes("float").columns
+    colonne_quali = data.select_dtypes("object").columns
+    
+    fig, axes = plt.subplots(nrows=3, ncols=len(colonne_quanti), figsize=(15, 6))
+
+    # Parcourez chaque colonne quantitative et affichez son histogramme
+    for i, col in enumerate(colonne_quanti):
+        sns.histplot(data[col].to_numpy(), kde=True, ax=axes[0, i], color="blue", bins=30)
+        axes[0, i].set_xlabel(col)
+        axes[0, i].set_ylabel('Fréquence')
+    
+    # Parcourez chaque colonne qualitative et affichez un diagramme en secteurs (pie chart)
+    for i, col in enumerate(colonne_quali):
+        counts = data[col].value_counts()
+        if i < len(colonne_quali) and i < 6:
+            row = 1
+        else:
+            row = 2
+            i -= 6  # Réduisez l'indice pour accéder correctement aux colonnes qualitatives dans la deuxième ligne
+        axes[row, i].pie(counts, labels=counts.index, autopct='%1.1f%%')
+        axes[row, i].set_xlabel(col)
+        axes[row, i].set_ylabel('')
+    for i in range(1,6):
+        axes[2,i].axis('off')    
+
+    # Ajustez la mise en page pour éviter les chevauchements d'étiquettes
+    plt.tight_layout()
+    # Affichez la figure
+    fig.suptitle("Figure Title")
+    plt.show() 
